e01f4f9c2eaf06fc711b727b5a8494cba50474db
Ewooral <boahen.cos@gmail.com>
2022-09-29 10:28:36 +0000

    fix: Add vscode autocomplete configuration to settings for css files

--- client/tailwind-react-app/tailwind/src/components/Navbar.tsx
+++ client/tailwind-react-app/tailwind/src/components/Navbar.tsx
@@ -1,36 +1,48 @@
-import React from 'react'
-import Logo from '../assets/img/logo.svg'
+import React from 'react';
+import Logo from '../assets/img/logo.svg';
 
 const Navbar = () => {
   return (
     <>
       <nav className="relative container p-6">
-
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-around">
           {/* logo */}
           <div className="pt-2">
             <img src={Logo} alt="logo" />
           </div>
 
+          <div className='bg'></div>
+
           {/* Menu Items */}
-          <div className="hidden sm:flex space-x-6">
-            <a href="#" className='hover:text-darkGrayishBlue'>Pricing</a>
-            <a href="#" className='hover:text-darkGrayishBlue'>Product</a>
-            <a href="#" className='hover:text-darkGrayishBlue'>About Us</a>
-            <a href="#" className='hover:text-darkGrayishBlue'>Careers</a>
-            <a href="#" className='hover:text-darkGrayishBlue'>Community</a>
+          <div className="hidden md:flex space-x-6">
+            <a href="#" className="hover:text-darkGrayishBlue">
+              Pricing
+            </a>
+            <a href="#" className="hover:text-darkGrayishBlue">
+              Product
+            </a>
+            <a href="#" className="hover:text-darkGrayishBlue">
+              About Us
+            </a>
+            <a href="#" className="hover:text-darkGrayishBlue">
+              Careers
+            </a>
+            <a href="#" className="hover:text-darkGrayishBlue">
+              Community
+            </a>
           </div>
-
           {/* Button */}
-          <a href="#" className="p-3 px-6 pt-2 text-white bg-brightRed 
-            rounded-full baseline">
-
+          <a
+            href="#"
+            className="p-3 px-6 pt-2 text-white bg-brightRed 
+            rounded-full baseline hover:bg-brightRedLight"
+          >
+            Get Started
           </a>
-
         </div>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
