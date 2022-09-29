8e5d28810451cc1cbd6f48882efcf64596e8f603
Ewooral <boahen.cos@gmail.com>
2022-09-29 09:37:25 +0000

    perf: Configure eslint and prettier in the react-typescript-tailwindcss app

--- client/tailwind-react-app/tailwind/src/components/Navbar.tsx
+++ client/tailwind-react-app/tailwind/src/components/Navbar.tsx
@@ -1,12 +1,35 @@
 import React from 'react'
+import Logo from '../assets/img/logo.svg'
 
 const Navbar = () => {
   return (
-   <>
-     <nav className="relative container p-6">
-       Hello world!
-     </nav>
-   </>
+    <>
+      <nav className="relative container p-6">
+
+        <div className="flex items-center justify-between">
+          {/* logo */}
+          <div className="pt-2">
+            <img src={Logo} alt="logo" />
+          </div>
+
+          {/* Menu Items */}
+          <div className="hidden sm:flex space-x-6">
+            <a href="#" className='hover:text-darkGrayishBlue'>Pricing</a>
+            <a href="#" className='hover:text-darkGrayishBlue'>Product</a>
+            <a href="#" className='hover:text-darkGrayishBlue'>About Us</a>
+            <a href="#" className='hover:text-darkGrayishBlue'>Careers</a>
+            <a href="#" className='hover:text-darkGrayishBlue'>Community</a>
+          </div>
+
+          {/* Button */}
+          <a href="#" className="p-3 px-6 pt-2 text-white bg-brightRed 
+            rounded-full baseline">
+
+          </a>
+
+        </div>
+      </nav>
+    </>
   )
 }
 
