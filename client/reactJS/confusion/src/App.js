21b12e43573d5680c6111f70ab431cde6b98d141
Ewooral <boahen.cos@gmail.com>
2022-10-02 08:10:00 +0000

    refactor: Move assets repository to the src repository

--- client/reactJS/confusion/src/App.js
+++ client/reactJS/confusion/src/App.js
@@ -1,76 +1,30 @@
-// import logo from './logo.svg';
-import './App.css';
-import { React, Component } from 'react';
-import ReactDOM from 'react-dom/client';
-import {Navbar, NavbarBrand} from 'reactstrap';
-import Menu from './components/Menu.js';
-import { DISHES } from './shared/dishes'
+import { Component } from "react";
+import './App.css'
+// import Logo from './assets/images/logo.png';
+import Menu from "./components/Menu";
+import { DISHES } from "./shared/dishes"
 
-// functional component
-function Start() {
-  const rootElement =
-    React.createElement("div", {},
-      React.createElement("h1", {}, "Bookmarks"),
-      React.createElement("ul", {},
-        React.createElement("li", {},
-          React.createElement("h2", {}, "Etherient"),
-          React.createElement("a",
-            { href: "https://www.etherient.com" },
-            "The home page of Etherient"
-          )
-        ),
-        React.createElement("li", {},
-          React.createElement("h2", {},
-            "Frank's Home"
-          ),
-          React.createElement("a",
-            { href: "https://www.zammetti.com" },
-            "The web home page of Frank Zammetti"
-          )
-        )
-      )
-    );
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
-    <>
-      <Start />
-    </>
-  )
-
-}
-
-// class component
-class App extends Component{
-
-  // LIFTING STATE UP IN REACT
-constructor(props){
+//  class component
+class App extends Component {
+  constructor(props) {
     super(props);
-
-    this.state = {
-      dishes: DISHES
+    this.state={
+      LISTS: DISHES
+        
     };
 
-}
-  render(){
-    return(
-        <div className="App">
-          <Navbar className="primary">
-            <div className="container">
-              <NavbarBrand href="/">
-              
-              
-                MY RESTAURANT
-                <Menu dishes={this.state.dishes} />
+  }
 
-              </NavbarBrand>
-            </div>
-          </Navbar>
-
+  render() {
+    return (
+      <>
+        <div className="relative container-fluid">
+          <Menu myList={this.state.LISTS} />
         </div>
+      </>
     )
+  }
 }
-}
 
+export default App
 
-
-export default App;
--- client/reactJS/confusion/src/components/Menu.js
+++ client/reactJS/confusion/src/components/Menu.js
@@ -1,42 +1,68 @@
 import React, { Component } from 'react';
-import { Media } from 'reactstrap';
-
 
 class Menu extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      selectList: null
 
     };
+    console.log("Menu component constructor is invoked!")
+  }
+  onListSelect(dish) {
+    this.setState({ selectList: dish })
+  }
+
+  componentDidMount(){
+    console.log("Menu component componentDidMount is invoked!")
+
+  }
+
+  renderDish(dish) {
+    if (dish != null) {
+      return (
+        <div className='flex justify-between 
+         p-3 gap-11 bg-red-900 text-white'>{dish.description}
+        </div>
+      )
+    }
+    else {
+      return (
+        <div></div>
+      )
+    }
   }
 
   render() {
-    const menu = this.props.dishes.map((dish) => {
+    const menu = this.props.myList.map((dish) => {
       return (
-        <div key={dish.id} className="col-12 mt-5">
-          <Media tag="li">
-            <Media left middle>
-              <Media object src={dish.image} alt={dish.name} />
-            </Media>
-            <Media body className="ml-5">
-              <Media heading>{dish.name}</Media>
-              <p>{dish.description}</p>
-            </Media>
-          </Media>
+        <div className='flex justify-between 
+         p-3 gap-11 cursor-pointer' key={dish.id}>
+          <div>
+            <h1 className='bg-blue-800 text-white p-2' onClick={() => this.onListSelect(dish)}>{dish.name}</h1>
+          </div>
+          <div className='hidden'>
+            <h5>{dish.description}</h5>
+          </div>
         </div>
       );
     });
+    console.log("Menu component render is invoked!")
 
     return (
-      <div className="container">
-        <div className="row">
-          <Media list>
-            {menu}
-          </Media>
+      <div>
+        <article>
+          {menu}
+        </article>
+        <div>
+          {this.renderDish(this.state.selectList)}
         </div>
       </div>
+
     );
+    
   }
+
 }
 
 export default Menu;
