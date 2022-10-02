65c8fd974c699766b0b79b20278d93130f47c16a
Ewooral <boahen.cos@gmail.com>
2022-10-02 01:51:01 +0000

    fix: Lift state up to the App.js component and ments

--- client/reactJS/confusion/src/App.js
+++ client/reactJS/confusion/src/App.js
@@ -1,23 +1,66 @@
 // import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+import { React, Component } from 'react';
+import ReactDOM from 'react-dom/client';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/Menu.js';
+import { DISHES } from './shared/dishes'
 
 // functional component
+function Start() {
+  const rootElement =
+    React.createElement("div", {},
+      React.createElement("h1", {}, "Bookmarks"),
+      React.createElement("ul", {},
+        React.createElement("li", {},
+          React.createElement("h2", {}, "Etherient"),
+          React.createElement("a",
+            { href: "https://www.etherient.com" },
+            "The home page of Etherient"
+          )
+        ),
+        React.createElement("li", {},
+          React.createElement("h2", {},
+            "Frank's Home"
+          ),
+          React.createElement("a",
+            { href: "https://www.zammetti.com" },
+            "The web home page of Frank Zammetti"
+          )
+        )
+      )
+    );
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <>
+      <Start />
+    </>
+  )
 
+}
 
 // class component
 class App extends Component{
-  render() {
+
+  // LIFTING STATE UP IN REACT
+constructor(props){
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+
+}
+  render(){
     return(
         <div className="App">
           <Navbar className="primary">
             <div className="container">
               <NavbarBrand href="/">
               
+              
                 MY RESTAURANT
-                <Menu />
+                <Menu dishes={this.state.dishes} />
 
               </NavbarBrand>
             </div>
@@ -25,7 +68,9 @@ class App extends Component{
 
         </div>
     )
-  }
 }
+}
+
+
 
 export default App;
