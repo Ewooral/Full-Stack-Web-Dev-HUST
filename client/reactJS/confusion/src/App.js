6a5b63072d3b857efde19ebb4d28a9d2eda32c4a
Ewooral <boahen.cos@gmail.com>
2022-10-10 15:30:55 +0000

    ...

--- client/reactJS/confusion/src/App.js
+++ client/reactJS/confusion/src/App.js
@@ -1,27 +1,16 @@
 import { Component } from "react";
+import Main from "./components/class_components/Main";
 import './App.css'
 // import Logo from './assets/images/logo.png';
-import Menu from "./components/Menu";
-import { DISHES, Names } from "./shared/dishes"
 
 //  class component
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      LISTS: DISHES,
-      NAMES: Names
-
-    };
-
-  }
+  
 
   render() {
     return (
       <>
-        <div className="relative container-fluid">
-          <Menu myList={this.state.LISTS} myName={this.state.NAMES} />
-        </div>  
+        <Main />
       </>
       
     )
--- client/reactJS/confusion/src/components/class_components/Main.js
+++ client/reactJS/confusion/src/components/class_components/Main.js
@@ -1,30 +1,37 @@
 // This is a sudo container component for our app. Thus the root container 
 
 import { Component } from "react";// import Logo from './assets/images/logo.png';
-import Menu from "./components/Menu";
-import { DISHES } from "./shared/dishes"
+import Menu from "./Menu";
+import { DISHES } from "../../shared/dishes"
 
 //  class component
-class App extends Component {
+class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            LISTS: DISHES
-
+            LISTS: DISHES,
+            selectList: null,
         };
 
     }
+
+    onListSelect(dishId) {
+        this.setState({ selectList: dishId })
+    }
+
 
     render() {
         return (
             <>
                 <div className="relative container-fluid">
-                    <Menu myList={this.state.LISTS} />
+                    <Menu myList={this.state.LISTS} 
+                        onClick={(dishId) => this.onListSelect(dishId)}
+                    />
                 </div>
             </>
         )
     }
 }
 
-export default App
+export default Main
 
