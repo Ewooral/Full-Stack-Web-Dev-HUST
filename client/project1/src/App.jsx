a24dfb4ddbef127700ca37fcb2b02be827611374
Ewooral <boahen.cos@gmail.com>
2022-10-12 08:20:55 +0000

    feat: Symbolic link has been created for node_modules folder to be used in all react apps

---
+++ client/project1/src/App.jsx
@@ -0,0 +1,65 @@
+import { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0,
+    }
+    console.log("Constructor is invoked!")
+  }
+
+
+  componentDidMount() {
+    console.log('componentDidMount works')
+  }
+
+  onChangeClick(val, nu) {
+    this.setState({
+      value: val + 1,
+    })
+  }
+  renderSomething(){
+    return(
+      <div className="">
+        
+      </div>
+    )
+  }
+
+
+
+  render() {
+    console.log("Render method is invoked!")
+
+    return (
+      <div className="">
+        <button onClick={() => {
+          this.onChangeClick(this.state.value)
+
+
+        }}
+          className='paintRed'>
+          <p>
+            Click
+          </p>
+          {this.state.value}
+
+        </button>
+
+
+      </div>
+    )
+  }
+
+  componentDidUpdate() {
+    console.log('componentdidupdate works fine!')
+  }
+
+}
+
+
+
+
+export default App
