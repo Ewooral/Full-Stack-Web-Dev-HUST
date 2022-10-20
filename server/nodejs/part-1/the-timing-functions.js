936687e0bcada8a47214ecec6ccf9d2362668614
Ewooral <boahen.cos@gmail.com>
2022-10-20 13:27:52 +0000

    ...

--- server/nodejs/part-1/the-timing-functions.js
+++ server/nodejs/part-1/the-timing-functions.js
@@ -10,9 +10,22 @@
 
 // clearInterval()
 
-const waitTime = 3000;
+const waitTime = 5000;
+const waitInterval = 500;
+let currentTime = 0;
+
+const incTime = () => {
+    currentTime += waitInterval;
+    console.log(`waiting ${currentTime / 1000} seconds`);
+}
+
 console.log(`setting a ${waitTime / 1000} second delay`);
 
-const timerFinished = () => console.log("done!");
+const timerFinished = () => {
+    clearInterval(interval);
+    console.log("done!");
+};
 
+const interval = setInterval(incTime, waitInterval);
 setTimeout(timerFinished, waitTime);
+
