756601c0cd1404c1f3343e4caeef675dd9f9f235
Ewooral <boahen.cos@gmail.com>
2022-11-03 17:05:49 +0000

    ...

--- server/nodejs/node-fundamentals/server.mjs
+++ server/nodejs/node-fundamentals/server.mjs
@@ -1,10 +1,145 @@
-import { format } from 'date-fns'
-import { v4 as uuid } from 'uuid';
+import http from 'http'
+import path from 'path';
+import url from 'url'
 import fs from 'fs'
+import fspromises from 'fs/promises'
+import { logEvents } from "./logEvents.mjs";
+import EventEmitter from 'events'
+class Emitter extends EventEmitter { }
+// initialize object
+const myEmitter = new Emitter()
 
-console.log(format(new Date(2022, 2, 11), 'MM/dd/yyyy\tHH:mm:ss'))
-console.log("Hello")
-
-console.log(uuid())
+let filename = url.fileURLToPath(import.meta.url)
+let __dirname = path.dirname(filename)
 
 
+
+// PORT
+const PORT = process.env.PORT || 5000;
+
+
+const serveFile = async (filePath, contentType, response) => {
+    try {
+        const data = await fspromises.readFile(filePath, 'utf-8')
+        response.writeHead(
+            200, 
+            { 'contentType': contentType }
+        )
+        response.end()
+
+    } catch (error) {
+        console.log(error)
+        response.statusCode = 500
+        response.end()
+    }
+}
+
+// SERVER
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method)
+
+    const extension = path.extname(req.url)
+
+    let contentType;
+
+    switch (extension) {
+        case '.css':
+            contentType = 'text/css'
+            break;
+        case '.json':
+            contentType = 'application/json'
+            break;
+        case '.js':
+            contentType = 'text/javascript'
+            break;
+        case '.jpg':
+            contentType = 'image/jpeg'
+            break;
+        case '.png':
+            contentType = 'image/png'
+            break;
+        case '.txt':
+            contentType = 'text/plain'
+            break;
+
+        default:
+            contentType: 'text/html'
+            break;
+    }
+
+    let filePath =
+        contentType === 'text/html' && req.url === '/'
+            ? path.join(__dirname, 'views', 'index.html')
+            : contentType === 'text/html' && req.url.slice(-1) === '/'
+                ? path.join(__dirname, 'views', req.url, 'index.html')
+                : contentType === 'text/html'
+                    ? path.join(__dirname, 'views', req.url)
+                    : path.join(__dirname, req.url)
+
+    // makes .html extension not required in the browser
+    if (!extension && req.url.slice(-1) !== '/') filePath += '.html'
+
+
+
+    const fileExits = fs.existsSync(filePath);
+    if (fileExits) {
+        // Serve the file
+        serveFile(filePath, contentType, res)
+    } else {
+        // 404
+        // 301 redirect
+        switch (path.parse(filePath).base) {
+            case 'old-page.html':
+                res.writeHead(301, { 'Location': '/new-page.html' })
+                res.end()
+                break
+            case 'www-page.html':
+                res.writeHead(301, { 'Location': '/' })
+                res.end()
+                break
+            default:
+                // Serve a 404 response
+                serveFile(path.join(__dirname, 'views', '404.html'),
+                    'text/html',
+                    res)
+            
+        }
+        console.log(path.parse(filePath))
+    }
+
+    // let filePath;
+
+    // if (req.url === '/' || req.url === 'index.html') {
+    //     res.statusCode = 200;
+    //     res.setHeader('Content-Type', 'test/html')
+
+    //     let filename = url.fileURLToPath(import.meta.url)
+    //     let __dirname = path.dirname(filename)
+    //     filePath = path.join(__dirname, 'views', 'index.html');
+
+    //     fs.readFile(
+    //         filePath,
+    //         'utf8',
+    //         (err, data) => {
+    //             res.end(data)
+    //         })
+
+    // }
+}
+)
+
+
+
+// LISTEN
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+
+
+
+
+
+//add listener for the log event
+// myEmitter.on('log', (msg) => logEvents(msg))
+
+//Emit event
+// myEmitter.emit('log', 'Log event emitted')
