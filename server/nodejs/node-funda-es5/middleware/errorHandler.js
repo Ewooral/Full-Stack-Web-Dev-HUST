9737044d68fec30e2964b2b6222b60086ad78b21
Ewooral <boahen.cos@gmail.com>
2022-11-16 23:35:31 +0000

    ...

---
+++ server/nodejs/node-funda-es5/middleware/errorHandler.js
@@ -0,0 +1,11 @@
+// const { logEvents } = require('./middleware/logEvents.js')
+
+
+// const errorHandler = (err, req, res, next) => {
+//     logEvents(`${err.name}: ${err.message}`, 'errLog.txt')
+//     console.error(err.stack)
+//     res.status(500).send(err.message)
+// }
+
+// module.exports = { errorHandler }
+
