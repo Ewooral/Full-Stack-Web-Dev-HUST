8cf0d4254fdf20155ffdcb1391fc1e0dd99c24f1
Ewooral <boahen.cos@gmail.com>
2023-03-07 17:42:35 +0000

    chore: Make a an http fetch call with fetch API

--- server/http/http.mjs
+++ server/http/http.mjs
@@ -1,24 +1,25 @@
 const apiKey = generateKey();
+const url = getURL()
+const settings = getSettings();
 
-const items = await getItemData();
+const response = await fetch(url, settings)
+const responseData = await response.json()
 
-logItems(items)
+logItems(responseData)
 
 
 
-async function getItemData() {
-    const response = await fetch("https://api.boot.dev/v1/courses_rest_api/learn-http/items", {
+function getSettings() {
+    return {
         method: 'GET',
         mode: 'cors',
         headers: {
             'X-API-Key': apiKey,
             'Content-Type': 'application/json'
         }
-    })
-    return response.json()
-}
+    }
 
-getItemData()
+}
 
 function generateKey() {
     const characters = 'ABCDEF0123456789'
@@ -33,4 +34,15 @@ function logItems(items) {
     for (const item of items) {
         console.log(item.name);
     }
-} 
+}
+
+function getURL() {
+    return "https://api.boot.dev/v1/courses_rest_api/learn-http/items"
+}
+
+/**
+ * The Fetch function is made available to us by the JS language running in
+ * the browser, all we have to do is provide it with the parameters it requires
+ * 
+ * It takes two inputs, URL and a settings OBJECT
+ */ 
