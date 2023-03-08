5e2336b1a508a021a649abd7a87e3a8d66869276
Ewooral <boahen.cos@gmail.com>
2023-03-08 21:12:16 +0000

    chore: Duplicate an array in JS

--- client/try.js
+++ client/try.js
@@ -1,11 +1,22 @@
-duplicate([1, 2, 3, 4, 5]); // [1,2,3,4,5,1,2,3,4,5]
+let a = [2, 3, 5]
+let b = a.slice()
+let c = [...b]
+console.log(b);
+
+b.push(8)
+console.log(a)
+console.log(b)
+console.log(c)
 
 const duplicate = (arr) => {
-    let arrCopy = arr.copy()
+    // let arrCopy = arr.slice()
+    let arrCopy = [...arr]
     for (let value of arrCopy) {
-        arr.append(value)
+        arr.push(value)
 
     }
     return arr
 
 }
+
+// duplicate([1, 2, 3, 4, 5]); // [1,2,3,4,5,1,2,3,4,5]
