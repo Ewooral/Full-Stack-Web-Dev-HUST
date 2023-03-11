6579565408d3bbb1a7f4f5077b72943be26fa57a
Ewooral <boahen.cos@gmail.com>
2023-03-11 23:42:25 +0000

    chore: Use fetch API to retrieve data

--- server/http/http.mjs
+++ server/http/http.mjs
@@ -72,9 +72,9 @@ if (!ipAddress) {
 
 console.log("..................................................................")
 fetch("https://api.boot.dev/v1/courses_rest_api/learn-http/items", {
-    // headers: {
-    'Content-Type': 'application/json'
-}
+    headers: {
+        'Content-Type': 'application/json'
+    }
 })
     .then((response) => response.json())
     .then((data) => data.forEach(element => {
