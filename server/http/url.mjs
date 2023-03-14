931971411c99b518c65e139d4a764bfd5988cf96
Ewooral <boahen.cos@gmail.com>
2023-03-14 02:33:49 +0000

    feat: Print URL parts

--- server/http/url.mjs
+++ server/http/url.mjs
@@ -1,3 +1,7 @@
 const printURLParts = (urlString) => {
     const urlObj = new URL(urlString)
+    console.log(urlObj.protocol)
 }
+
+
+const fantasyQuestURL = 'http://dragonslayer:pwn3d@fantasyquest.com:8080/maps?sort=rank#id'
